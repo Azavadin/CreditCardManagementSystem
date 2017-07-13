10331214b7a71837c40179f0aaf7a52ed58e578b
Azavadin <scharantejams@gmail.com>
2017-07-13 02:24:50 -0600

    Initial commit

---
+++ DAL/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using CreditCardManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CreditCardManagementSystem.DAL
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CreditCardManagementSystem.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string InterchangeRate { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+
+    }
+}
